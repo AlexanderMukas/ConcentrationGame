1816c449dfec4eabfd24057029b9421a2ede023f
Alexander Mukas <alexandermukas@yahoo.com>
2022-10-17 20:08:52 +0300

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Alexander Mukas on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //some code
+    }
+
+
+}
+
