2f650a2b47abcafa60fac01e9de74884a62588cb
Alexander Mukas <alexandermukas@yahoo.com>
2022-10-17 20:58:29 +0300

    prepare to coding commit

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //some code
-        // some code again...
+    
+    
+    
+    
+    
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLabel: UILabel!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        
     }
-    /*
-     Dev branch
-     */
-
-
+    
+    
+    
+    
 }
 
