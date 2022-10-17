db15d842a8bb4ce9631c9f0b530313f6b81da926
Alexander Mukas <alexandermukas@yahoo.com>
2022-10-18 01:01:40 +0300

    add flipping of card on emoji

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,16 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var touches: Int = 0 {
+        // observer prop
+        didSet {
+            print("Now \(touches) taps...")
+            touchLabel.text = "Touches : \(touches)"
+        }
+    }
     
     
     
+    // function of flipping card
+    func flipbutton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.systemBlue
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
+    
+    
+    //@IBOutlet weak var resetBtn: UIButton!
+    @IBAction func resetTouches(_ sender: UIButton) {
+        touches = 0
+        touchLabel.text = "Touches : \(touches)"
+    }
     
     
     
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var touchLabel: UILabel!
     @IBAction func buttonAction(_ sender: UIButton) {
-        
+        touches += 1
+        flipbutton(emoji: "🦊", button: sender)
         
     }
     
