7745f82f46deb8eed47dcabfb34039b688d80dec
Alexander Mukas <alexandermukas@yahoo.com>
2022-10-28 00:31:12 +0300

    fix reset button funcionality

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     var touches: Int = 0 {
-        // observer prop
         didSet {
             print("Now \(touches) taps...")
             touchLabel.text = "Touches : \(touches)"
@@ -21,9 +20,7 @@ class ViewController: UIViewController {
     
     // function of flipping card
     func flipbutton(emoji: String, button: UIButton) {
-        
         button.titleLabel?.font = .systemFont(ofSize: 65)
-        
         if button.currentTitle == emoji {
             button.setTitle("", for: .normal)
             button.backgroundColor = UIColor.systemBlue
@@ -35,20 +32,22 @@ class ViewController: UIViewController {
     
     let emojiCollection: Array = ["🦊", "🐰", "🦊", "🐰"]
     
-    
-    //@IBOutlet weak var resetBtn: UIButton!
     @IBAction func resetTouches(_ sender: UIButton) {
         touches = 0
         touchLabel.text = "Touches : \(touches)"
+        //var button = ""
+        for button in buttonCollection {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.systemBlue
+        }
     }
-    
     
     
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var touchLabel: UILabel!
+    
     @IBAction func buttonAction(_ sender: UIButton) {
         touches += 1
-        
         // index button inject emoji index in array
         if let buttonIndex = buttonCollection.firstIndex(of: sender) {
             flipbutton(
@@ -57,9 +56,6 @@ class ViewController: UIViewController {
         }
         
     }
-    
-    
-    
     
 }
 
